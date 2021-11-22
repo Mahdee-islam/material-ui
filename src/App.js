ea18a63517bbc86fa90d575c061a8f6f60eff149
Mahdi <hirelingmahdi@gmail.com>
2021-11-22 21:23:54 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Favorite from '@mui/icons-material/Favorite';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import SaveIcon from '@mui/icons-material/Save';
+import SendIcon from '@mui/icons-material/Send';
+import { Button, ButtonGroup, Checkbox, createTheme, Grid, IconButton, Paper, TextField } from '@mui/material';
+import { orange } from '@mui/material/colors';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+function CheckBoxExample() {
+  return (
+    <div style = {{ border: '2px solid green'}}>
+       <h1>This is checkbox</h1>
+       <h2>lorem</h2>
+       <div>
+      <Checkbox {...label} icon={<FavoriteBorder />} color= "warning" checkedIcon={<Favorite />} />
+      <Checkbox 
+      color = "secondary"
+        {...label}
+        icon={<BookmarkBorderIcon />}
+        checkedIcon={<BookmarkIcon />}
+      />
+    </div>
+    </div>
+  )
+}
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+   
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme = {theme} >
+      <div className="App">
+      <CheckBoxExample/>
+      <br/>
+      <div style = { { border : '2px solid red'}} >
+      <h2>This is Button</h2>
+      <ButtonGroup variant = "contained">
+      <Button onClick = {() => alert('save successfully')} startIcon = { <SaveIcon/> }   >Save</Button>
+      <br/>
+      <Button onClick = {() => alert('delete successfully')} startIcon = { <DeleteIcon/> }   >Delete</Button>
+      <br/>
+      <Button onClick = {() => alert('sent successfully')} endIcon = { <SendIcon/> }   >Send</Button>
+      </ButtonGroup>
+      <IconButton aria-label="delete">
+  <DeleteIcon />
+</IconButton>
+<IconButton color="secondary" aria-label="add an alarm">
+  <AlarmIcon />
+  </IconButton>
+      </div>
+      <div>
+      <TextField id="standard-basic" label="Search" variant="filled" />
+      </div>
     </div>
+    <Grid container spacing = { 2 }  justifyContent="center">
+      <Grid item xs={3} md = {6} lg = {12}>
+        <Paper style= {{
+         height: '50%',
+         width: "50%",
+        }}>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ratione velit, ad omnis qui dolor dignissimos, libero repellat animi sunt illum eos enim voluptates eaque rerum neque quo! Dolores, enim!</p>
+        </Paper>
+      </Grid>
+      <Grid item xs={3} md = {6} lg = {12}>
+        <Paper style= {{
+          height: '50%',
+          width: "50%",
+        }}>
+          <p>lorem</p>
+        </Paper>
+      </Grid>
+      <Grid item xs={3} md = {6} lg = {12}>
+        <Paper style= {{
+          height: '50%',
+          width: "50%",
+        }}>
+          <p>lorem</p>
+        </Paper>
+      </Grid>
+    </Grid>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Grid.js
@@ -0,0 +1,16 @@
+import { Paper } from '@mui/material';
+import React from 'react';
+
+const Grid = () => {
+    return (
+        <div>
+            <Grid container >
+                <Grid item>
+                    <Paper></Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
+export default Grid;
